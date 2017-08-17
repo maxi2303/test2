bc9c2fa04347483f0b7ce4737bfe668071793832
alumno <alumno>
2017-08-17 17:56:23 -0300

    programando primera clase

---
+++ src/Prueba.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+ 
+ int numeroUno;
+ int numeroDos;
+int resultado;
+String numcadena=1;
+int numentero=integer.parseint(numcadena);
+Scanner scanner= new Scanner(System.in); 
+ 
+ System.out.println ("ingrese un numero");
+ numeroUno=Scanner.nextLine();
+ System.out.println("ingrese segundo numero");
+ numeroDos=Scanner.nextLine();
+ System.out.println.("el resultado es: "+resultado);
+ 
+		 
+ 
+	}
+
+}
